f7017b79c9022218c81423e687e8f0305cdc7c99
Stephen Asbury <sasbury@nats.io>
2018-09-05 11:09:09 -0700

    Update connect_options
    Added comment about using timer

--- connect_options.rb
+++ connect_options.rb
@@ -1,4 +1,5 @@
 # [begin connect_options]
+# There is currently no connect timeout as part of the Ruby NATS client API, but you can use a timer to mimic it.
 require 'nats/client'
 
 timer = EM.add_timer(5) do
